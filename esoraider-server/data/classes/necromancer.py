555165c443e87a7f1d17a510fe07f5c40a25c1d5
Dreemurro <dr33murro@gmail.com>
2021-08-01 17:52:12 +0300

    Detonating Siphon tracking

--- esoraider-server/data/classes/necromancer.py
+++ esoraider-server/data/classes/necromancer.py
@@ -22,3 +22,20 @@ class NECROMANCER_SKILLS(EsoEnum):
         children=[UNNERVING_BONEYARD_DAMAGE],
         icon='https://assets.rpglogs.com/img/eso/abilities/ability_necromancer_004_a.png',
     )
+    DETONATING_SIPHON_EXPLOSION = Skill(
+        name='Detonating Siphon',
+        id=123082,
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_necromancer_005_b.png',
+    )
+    DETONATING_SIPHON_DAMAGE = Skill(
+        name='Detonating Siphon',
+        id=118766,
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_necromancer_005_b.png',
+    )
+    DETONATING_SIPHON = Skill(
+        name='Detonating Siphon',
+        id=118763,
+        link='https://eso-hub.com/en/skills/necromancer/grave-lord/detonating-siphon',
+        children=[DETONATING_SIPHON_DAMAGE, DETONATING_SIPHON_EXPLOSION],
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_necromancer_005_b.png',
+    )
