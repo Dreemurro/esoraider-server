18546ffb7f6aa560562975bdf25a15c9da7b32a0
Dreemurro <dr33murro@gmail.com>
2021-08-01 17:51:37 +0300

    Unnerving Boneyard tracking

--- esoraider-server/data/classes/necromancer.py
+++ esoraider-server/data/classes/necromancer.py
@@ -1,4 +1,4 @@
-from data.core import EsoEnum
+from data.core import EsoEnum, Skill
 
 
 class NECROMANCER_BUFFS(EsoEnum):
@@ -10,4 +10,15 @@ class NECROMANCER_DEBUFFS(EsoEnum):
 
 
 class NECROMANCER_SKILLS(EsoEnum):
-    pass
+    UNNERVING_BONEYARD_DAMAGE = Skill(
+        name='Unnerving Boneyard',
+        id=117809,
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_necromancer_004_a.png',
+    )
+    UNNERVING_BONEYARD = Skill(
+        name='Unnerving Boneyard',
+        id=117805,
+        link='https://eso-hub.com/en/skills/necromancer/grave-lord/unnerving-boneyard',
+        children=[UNNERVING_BONEYARD_DAMAGE],
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_necromancer_004_a.png',
+    )
