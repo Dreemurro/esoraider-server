7819ef886cfb485ed0006b697ef430318d154ea7
Dreemurro <dr33murro@gmail.com>
2021-08-14 15:50:12 +0300

    Vampire's Bane tracking

--- esoraider-server/data/classes/templar/debuffs.py
+++ esoraider-server/data/classes/templar/debuffs.py
@@ -1,5 +1,9 @@
-from data.core import EsoEnum
+from data.core import Debuff, EsoEnum
 
 
 class TEMPLAR_DEBUFFS(EsoEnum):
-    pass
+    VAMPIRES_BANE = Debuff(
+        name='Vampire\'s Bane',
+        id=21731,
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_templar_vampire_bane.png',
+    )
--- esoraider-server/data/classes/templar/skills.py
+++ esoraider-server/data/classes/templar/skills.py
@@ -1,5 +1,14 @@
-from data.core import EsoEnum
+from data.classes.templar.debuffs import TEMPLAR_DEBUFFS
+from data.core import EsoEnum, Skill
 
 
 class TEMPLAR_SKILLS(EsoEnum):
-    pass
+    VAMPIRES_BANE = Skill(
+        # 21729 - skill and initial damage (debuff), 21731 - DoT (also debuff)
+        # Mark them as children or move to debuffs?
+        name='Vampire\'s Bane',
+        id=21729,
+        debuffs=[TEMPLAR_DEBUFFS.VAMPIRES_BANE.value],
+        link='https://eso-hub.com/en/skills/templar/dawns-wrath/vampires-bane',
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_templar_vampire_bane.png',
+    )
