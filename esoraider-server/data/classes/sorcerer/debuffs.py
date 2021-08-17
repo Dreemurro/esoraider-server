5461a348b125f44a04c42109f6dcb9ae3cfa1997
Dreemurro <dr33murro@gmail.com>
2021-08-17 18:18:18 +0300

    Daedric Prey tracking

--- esoraider-server/data/classes/sorcerer/debuffs.py
+++ esoraider-server/data/classes/sorcerer/debuffs.py
@@ -1,5 +1,9 @@
-from data.core import EsoEnum
+from data.core import Debuff, EsoEnum
 
 
 class SORCERER_DEBUFFS(EsoEnum):
-    pass
+    DAEDRIC_PREY = Debuff(
+        name='Daedric Prey',
+        id=24328,  # Debuff ID = Skill ID, don't ask ¯\_(ツ)_/¯
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_sorcerer_explosive_curse.png',
+    )
--- esoraider-server/data/classes/sorcerer/skills.py
+++ esoraider-server/data/classes/sorcerer/skills.py
@@ -1,5 +1,12 @@
-from data.core import EsoEnum
+from data.classes.sorcerer.debuffs import SORCERER_DEBUFFS
+from data.core import EsoEnum, Skill
 
 
 class SORCERER_SKILLS(EsoEnum):
-    pass
+    DAEDRIC_PREY = Skill(
+        name='Daedric Prey',
+        id=24328,
+        debuffs=[SORCERER_DEBUFFS.DAEDRIC_PREY.value],
+        link='https://eso-hub.com/en/skills/sorcerer/daedric-summoning/daedric-prey',
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_sorcerer_explosive_curse.png',
+    )
