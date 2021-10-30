7cd23d46dfe7c68203f8e9cb1f467f69fd41c3ec
Dreemurro <dr33murro@gmail.com>
2021-10-30 17:51:23 +0300

    Track necromancer passives Only missing *Disdain Harm* and *Death Knell*

---
+++ esoraider-server/data/classes/necromancer/passives.py
@@ -0,0 +1,69 @@
+from data.core import Buff, EsoEnum
+
+
+class NECROMANCER_PASSIVES(EsoEnum):
+    # Bone Tyrant
+    DEATH_GLEANING = Buff(
+        name='Death Gleaning',
+        id=116235,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_005.png',
+        link='https://eso-hub.com/en/skills/necromancer/bone-tyrant/death-gleaning',
+    )
+    HEALTH_AVARICE = Buff(
+        name='Health Avarice',
+        id=116270,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_007.png',
+        link='https://eso-hub.com/en/skills/necromancer/bone-tyrant/health-avarice',
+    )
+    LAST_GASP = Buff(
+        name='Last Gasp',
+        id=116272,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_008.png',
+        link='https://eso-hub.com/en/skills/necromancer/bone-tyrant/last-gasp',
+    )
+
+    # Grave Lord
+    REUSABLE_PARTS = Buff(
+        name='Reusable Parts',
+        id=116189,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_001.png',
+        link='https://eso-hub.com/en/skills/necromancer/grave-lord/reusable-parts',
+    )
+    DISMEMBER = Buff(
+        name='Dismember',
+        id=116194,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_003.png',
+        link='https://eso-hub.com/en/skills/necromancer/grave-lord/dismember',
+    )
+    RAPID_ROT = Buff(
+        name='Rapid Rot',
+        id=116201,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_004.png',
+        link='https://eso-hub.com/en/skills/necromancer/grave-lord/rapid-rot',
+    )
+
+    # Living Death
+    CURATIVE_CURSE = Buff(
+        name='Curative Curse',
+        id=116287,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_009.png',
+        link='https://eso-hub.com/en/skills/necromancer/living-death/curative-curse',
+    )
+    NEAR_DEATH_EXPERIENCE = Buff(
+        name='Near-Death Experience',
+        id=116275,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_010.png',
+        link='https://eso-hub.com/en/skills/necromancer/living-death/near-death-experience',
+    )
+    CORPSE_CONSUMPTION = Buff(
+        name='Corpse Consumption',
+        id=116285,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_011.png',
+        link='https://eso-hub.com/en/skills/necromancer/living-death/corpse-consumption',
+    )
+    UNDEAD_CONFEDERATE = Buff(
+        name='Undead Confederate',
+        id=116283,
+        icon='https://assets.rpglogs.com/img/eso/abilities/passive_necromancer_012.png',
+        link='https://eso-hub.com/en/skills/necromancer/living-death/undead-confederate',
+    )
