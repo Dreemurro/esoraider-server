1e00e1f4b21259dbeb5ca5a7547f2aa8a86e524b
Dreemurro <dr33murro@gmail.com>
2021-10-30 17:56:11 +0300

    Track Dark Elf passives

--- esoraider-server/data/races.py
+++ esoraider-server/data/races.py
@@ -15,3 +15,23 @@ class RacialPassives(EsoEnum):
         icon='https://assets.rpglogs.com/img/eso/abilities/ability_sorcerer_013.png',
         link='https://eso-hub.com/en/skills/racial/breton-skills/spell-attunement',
     )
+
+    # Dark Elf (excluding Ashlander)
+    DYNAMIC = Buff(
+        name='Dynamic',
+        id=45267,
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_weapon_023.png',
+        link='https://eso-hub.com/en/skills/racial/dark-elf-skills/dynamic',
+    )
+    RESIST_FLAME = Buff(
+        name='Resist Flame',
+        id=45270,
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_sorcerer_062.png',
+        link='https://eso-hub.com/en/skills/racial/dark-elf-skills/resist-flame',
+    )
+    RUINATION = Buff(
+        name='Ruination',
+        id=45272,
+        icon='https://assets.rpglogs.com/img/eso/abilities/ability_sorcerer_010.png',
+        link='https://eso-hub.com/en/skills/racial/dark-elf-skills/ruination',
+    )
