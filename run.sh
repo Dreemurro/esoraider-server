3bbe62fa725ab99a96fdbde13a9c551a0f822cde
Dreemurro <dr33murro@gmail.com>
2023-02-24 14:25:31 +0300

    Replace pipenv with poetry and dockerize the app

--- run.sh
+++
@@ -1,1 +0,0 @@
-gunicorn --keyfile=./privkey.pem --certfile=./fullchain.pem -w 4 -k uvicorn.workers.UvicornWorker -b :5000 esoraider_server.app:app --daemon --log-file=./log.log --capture-output --log-level debug
