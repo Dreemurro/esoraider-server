ac5d2e1bdd22307d04fc42b99baa52af58aafb24
Dreemurro <dr33murro@gmail.com>
2024-12-02 18:21:15 +0700

    wip: replace requests-oauthlib with aioauth-client

--- esoraider_server/esologs/base.py
+++ esoraider_server/esologs/base.py
@@ -1,33 +1,22 @@
+from aioauth_client import OAuth2Client
 from gql import Client  # type: ignore
 from gql.dsl import DSLSchema  # type: ignore
 from gql.transport.aiohttp import AIOHTTPTransport  # type: ignore
 from gql.transport.exceptions import TransportQueryError  # type: ignore
 from loguru import logger
-from oauthlib.oauth2 import BackendApplicationClient  # type: ignore
-from requests_oauthlib import OAuth2Session  # type: ignore
 
 from esoraider_server.settings import CLIENT_ID, CLIENT_SECRET
-
-WAIT_FOR = 300
-TIMEOUT = 10.0
 
 
 class ApiWrapperBase(object):
     def __init__(self) -> None:
-        self._token = self._auth()
-        self._transport = AIOHTTPTransport(
-            url='https://www.esologs.com/api/v2/client',
-            headers={'Authorization': 'Bearer {0}'.format(self._token)},
-        )
-        self._client = Client(
-            transport=self._transport,
-            fetch_schema_from_transport=True,
-        )
+        self._client: Client | None = None
         self._session = None
         self.ds: DSLSchema = None
 
     async def connect(self):
         logger.info('Opening connection')
+        self._client = await self._create_client()
         self._session = await self._client.connect_async(reconnecting=True)
         self.ds = DSLSchema(self._client.schema)
         logger.info('Connected to API')
@@ -47,14 +36,25 @@ class ApiWrapperBase(object):
 
         return answer
 
-    def _auth(self):
-        access_token_url = 'https://www.esologs.com/oauth/token'  # noqa: S105
+    async def _create_client(self) -> Client:
+        token = await self._get_access_token()
+        return Client(
+            transport=AIOHTTPTransport(
+                url='https://www.esologs.com/api/v2/client',
+                headers={'Authorization': 'Bearer {0}'.format(token)},
+            ),
+            fetch_schema_from_transport=True,
+        )
 
-        client = BackendApplicationClient(client_id=CLIENT_ID)
-        oauth = OAuth2Session(client=client)
-        token = oauth.fetch_token(
-            token_url=access_token_url,
+    async def _get_access_token(self) -> str:
+        url = 'https://www.esologs.com/oauth/{0}'
+        client = OAuth2Client(
             client_id=CLIENT_ID,
             client_secret=CLIENT_SECRET,
+            authorize_url=url.format('authorize'),
+            access_token_url=url.format('token'),
         )
-        return token['access_token']
+        token, _ = await client.get_access_token(
+            '', grant_type='client_credentials',
+        )
+        return token
