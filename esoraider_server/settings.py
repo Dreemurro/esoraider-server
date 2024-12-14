560d0b5ecba73c2f0f89ebeca5fdb141ead0d917
Dreemurro <dr33murro@gmail.com>
2024-12-14 19:48:42 +0700

    chore: add default settings

--- esoraider_server/settings.py
+++ esoraider_server/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+CLIENT_ID = os.environ.get('CLIENT_ID', '')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET', '')
 DEBUG = os.environ.get('DEBUG') == 'True'
-HEALTHCHECK_TOKEN = os.environ.get('HEALTHCHECK_TOKEN')
+HEALTHCHECK_TOKEN = os.environ.get('HEALTHCHECK_TOKEN', 'braindead')
